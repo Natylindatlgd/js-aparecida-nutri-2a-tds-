2fc368d77c4e842374777682cbc513776f0ee915
Natylindatlgd <vitoria.pereira.nathalya@escola.pr.gov.br>
2024-11-04 12:27:09 -0300

    carga_adicionar-dados

--- js-aparecida-nutri-2a-tds--main/js/principal.js
+++ js-aparecida-nutri-2a-tds--main/js/principal.js
@@ -37,40 +37,44 @@ for (var i = 0; i < trPacientes.length; i++) {
         tdImc.textContent = "Altura e/ou peso inválidos!"
     }
 }
+
 titulo.addEventListener("click", mostraMensagem)
 function mostraMensagem() {
     console.log("Olá eu fui clicado!");
 }
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click"), function (event) {
+
+var botaoadicionar = document.querySelector("#adicionar-paciente");
+botaoadicionar.addEventListener("click",function(event) {
     event.preventDefault();
-    //console.log("Oi, cliquei no botão.");
+    //console.log("Oi,cliquei no botão");
 
-    var form = documen.querySelector("#fom-adiciona");
+var form = document.querySelector("#form-adiciona");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var Altura = form.altura.value;
-    var gordura = form.gordura.value;
+var nome = form.nome.value;
+var peso = form.peso.value;
+var altura = form.altura.value;
+var gordura = form.gordura.value;
 
-    var pacienteTr = document.createElement("td");
+var pacientetr = document.createElement("TR");
 
-    var nomeTd = document.createElement("Td");
-    var peso = document.createElement("Td");
-    var altura = document.createElement("Td");
-    var gordura = document.createElement("Td");
-    var Imctd = document.createElement("Td");
+var nometd = document.createElement("td");
+var pesotd = document.createElement("td");
+var alturatd = document.createElement("td");
+var gorduratd = document.createElement("td");
+var imctd = document.createElement("td");
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    altura.textContent = altura;
-    gordura.textContent = gordura;
+nometd.textContent = nome;
+pesotd.textContent = peso;
+alturatd.textContent = altura;
+gordura.textContent = gordura; 
 
-    pacienteTr.appendChild(nomeTd)
-    pacienteTr.appendChild(pesoTd)
-    pacienteTr.appendChild(altura)
-    pacienteTr.appendChild(gordura)
+pacientetr.appendChild(nometd);
+pacientetr.appendChild(pesotd);
+pacientetr.appendChild(alturatd);
+pacientetr.appendChild(gorduratd);
 
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr)
-};
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.appendChild(pacientetr);
+
+});
